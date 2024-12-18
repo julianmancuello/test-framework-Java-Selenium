fffa4c95228124b058bc226594cf936bfa25cad2
julianmancuello <162643807+julianmancuello@users.noreply.github.com>
2024-12-18 20:56:07 +0100

    removed useless imports

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -2,7 +2,6 @@ package pages;
 
 import context.ContextStore;
 import org.openqa.selenium.By;
-import org.openqa.selenium.ElementNotInteractableException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -13,8 +12,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.NoSuchElementException;
-
-import static common.Utils.*;
 
 public class BasePage {
 

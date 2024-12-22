df2051bfe74e23c3fedd3628ba9b946ee1dad6f3
julianmancuello <162643807+julianmancuello@users.noreply.github.com>
2024-12-22 19:23:02 +0100

    cleaned output

--- src/test/java/extensions/AfterTestExecutionLogger.java
+++ src/test/java/extensions/AfterTestExecutionLogger.java
@@ -1,7 +1,5 @@
 package extensions;
 
-import org.junit.jupiter.api.extension.AfterAllCallback;
-import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestWatcher;
 
@@ -9,63 +7,25 @@ import java.util.Optional;
 
 import static extensions.ReportFormat.*;
 
-public class AfterTestExecutionLogger implements TestWatcher, BeforeAllCallback, AfterAllCallback {
-
-    private static int testsExecuted = 0;
-    private static int testsSuccessful = 0;
-    private static int testsFailed = 0;
-    private static int testsAborted = 0;
-    private static int testsDisabled = 0;
-
-    @Override
-    public void beforeAll(ExtensionContext context) {
-        testsExecuted = 0;
-        testsSuccessful = 0;
-        testsFailed = 0;
-        testsAborted = 0;
-        testsDisabled = 0;
-    }
+public class AfterTestExecutionLogger implements TestWatcher {
 
     @Override
     public void testDisabled(ExtensionContext context, Optional<String> reason) {
-        String testName = context.getDisplayName();
         System.out.printf("%sTest result:%s [%sDISABLED%s]%n", CYAN_BOLD, RESET, YELLOW_BOLD, RESET);
-        testsExecuted++;
-        testsDisabled++;
     }
 
     @Override
     public void testSuccessful(ExtensionContext context) {
-        String testName = context.getDisplayName();
         System.out.printf("%sTest result:%s [%sSUCCESS%s]%n", CYAN_BOLD, RESET, GREEN_BOLD, RESET);
-        testsExecuted++;
-        testsSuccessful++;
     }
 
     @Override
     public void testAborted(ExtensionContext context, Throwable cause) {
-        String testName = context.getDisplayName();
         System.out.printf("%sTest result:%s [%sABORTED%s]%n", CYAN_BOLD, RESET, PEACH_BOLD, RESET);
-        testsExecuted++;
-        testsAborted++;
     }
 
     @Override
     public void testFailed(ExtensionContext context, Throwable cause) {
-        String testName = context.getDisplayName();
         System.out.printf("%sTest result:%s [%sFAILED%s]%n", CYAN_BOLD, RESET, RED_BOLD, RESET);
-        testsExecuted++;
-        testsFailed++;
-    }
-
-    @Override
-    public void afterAll(ExtensionContext context) {
-        System.out.println(BOLD + "\n----- Class Test Execution Summary -----");
-        System.out.printf("Tests executed: %d%n", testsExecuted);
-        System.out.printf("Tests successful: %d%n", testsSuccessful);
-        System.out.printf("Tests failed: %d%n", testsFailed);
-        System.out.printf("Tests aborted: %d%n", testsAborted);
-        System.out.printf("Tests disabled: %d%n", testsDisabled);
-        System.out.println("----------------------------------------" + RESET);
     }
 }
--- src/test/java/extensions/ReportFormat.java
+++ src/test/java/extensions/ReportFormat.java
@@ -8,7 +8,5 @@ public class ReportFormat {
     public static final String PEACH_BOLD = "\u001B[38;5;214;1m";
     public static final String YELLOW_BOLD = "\u001B[33;1m";
     public static final String MAGENTA_BOLD = "\u001B[1;35m";
-    public static final String PINK_BOLD = "\u001B[1;95m";
-    public static final String BOLD = "\u001B[1m";
     public static final String RESET = "\u001B[0m";
 }

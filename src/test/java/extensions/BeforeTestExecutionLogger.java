0cce0b00814fab537b06c75c69c81ade08966635
julianmancuello <162643807+julianmancuello@users.noreply.github.com>
2025-01-30 12:32:46 +0100

    test pipeline

--- src/test/java/extensions/BeforeTestExecutionLogger.java
+++ src/test/java/extensions/BeforeTestExecutionLogger.java
@@ -15,4 +15,3 @@ public class BeforeTestExecutionLogger implements BeforeTestExecutionCallback {
         System.out.println("----------------------------------------------------");
     }
 }
-//test
